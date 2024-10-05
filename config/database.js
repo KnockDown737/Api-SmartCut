05edae214278200e4128ddf801bf57cafd90cb9c
André Martins <93719914+KnockDown737@users.noreply.github.com>
2024-10-05 18:04:42 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
 });
+
 module.exports = sequelize;
