d8528fee07d85ca47dfe5ab523fcb0d2ef3bb57a
André Martins <93719914+KnockDown737@users.noreply.github.com>
2024-10-06 15:43:15 -0300

    Update agendamentos.js

--- routes/agendamentos.js
+++ routes/agendamentos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const Cliente = require('../models/cliente');
+const Cliente = require('../models/Cliente');
 const router = express.Router();
 
 // Criar um novo cliente
