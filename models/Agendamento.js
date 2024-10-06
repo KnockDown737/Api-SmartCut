8369bc783252a30566393e1908072bcaa77295e2
André Martins <93719914+KnockDown737@users.noreply.github.com>
2024-10-05 21:23:39 -0300

    Update Agendamento.js

--- models/Agendamento.js
+++ models/Agendamento.js
@@ -15,7 +15,7 @@ const Agendamento = sequelize.define('Agendamento', {
   },
   status: {
     type: DataTypes.ENUM('aberto', 'concluido', 'cancelado'),  // Enum para diferentes status
-    allowNull: false,
+    allowNull: true,
     defaultValue: 'aberto',  // O status padrão será 'aberto'
   }
 });
