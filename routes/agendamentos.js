fa75dc9f0231b85d4ed1f864c50fa91e603b25f9
André Martins <93719914+KnockDown737@users.noreply.github.com>
2024-10-05 23:48:04 -0300

    Update agendamentos.js

--- routes/agendamentos.js
+++ routes/agendamentos.js
@@ -2,29 +2,42 @@ const express = require('express');
 const Agendamento = require('../models/Agendamento');
 const Servico = require('../models/Servico');
 const Profissional = require('../models/Profissional');
+const Cliente = require('../models/Cliente'); // Certifique-se de importar o modelo de Cliente
 const router = express.Router();
 
-
 router.post('/', async (req, res) => {
-  const { data, horario, servicoId, profissionalId } = req.body;
+  const { data, horario, servicoId, profissionalId, status, clienteId } = req.body;
 
   try {
-    
+    // Verifique se o serviço existe
+    const servico = await Servico.findByPk(servicoId);
+    if (!servico) {
+      return res.status(404).json({ error: 'Serviço não encontrado' });
+    }
+
+    // Verifique se o profissional existe
+    const profissional = await Profissional.findByPk(profissionalId);
+    if (!profissional) {
+      return res.status(404).json({ error: 'Profissional não encontrado' });
+    }
+
+    // Verifique se o cliente existe
+    const cliente = await Cliente.findByPk(clienteId);
+    if (!cliente) {
+      return res.status(404).json({ error: 'Cliente não encontrado' });
+    }
+
+    // Cria o agendamento com os dados fornecidos
     const agendamento = await Agendamento.create({
       data,
       horario,
       ServicoId: servicoId,
       ProfissionalId: profissionalId,
+      status, // Incluindo o status do agendamento
+      ClienteId: clienteId // Incluindo o ClienteId
     });
 
-    
-    const servico = await Servico.findByPk(servicoId);
-
-    if (!servico) {
-      return res.status(404).json({ error: 'Serviço não encontrado' });
-    }
-
-   
+    // Resposta com detalhes do agendamento e o nome do serviço
     res.json({
       agendamento,
       servicoNome: servico.nome,
@@ -36,23 +49,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-
 router.get('/', async (req, res) => {
   try {
-
     const agendamentos = await Agendamento.findAll({
       include: [
         {
           model: Servico,
-          attributes: ['nome', 'preco'], 
+          attributes: ['nome', 'preco'],
         },
         {
           model: Profissional,
-          attributes: ['nome'], 
+          attributes: ['nome'],
+        },
+        {
+          model: Cliente, // Inclui também o Cliente na resposta
+          attributes: ['nome'],
         },
       ],
-    }); 
-
+    });
 
     res.json(agendamentos);
   } catch (error) {
