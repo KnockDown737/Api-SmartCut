59096e134f817f4e220c57dfe1d310fb34077017
André Martins <93719914+KnockDown737@users.noreply.github.com>
2024-10-05 22:17:15 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,20 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE, 
-  process.env.MYSQL_USER, 
-  process.env.MYSQL_PASSWORD, 
-  {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT || 3306,  
-    dialect: process.env.MYSQL_DIALECT,
-  }
-);
+try {
+  const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE, 
+    process.env.MYSQL_USER, 
+    process.env.MYSQL_PASSWORD, 
+    {
+      host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_PORT || 3306,
+      dialect: process.env.MYSQL_DIALECT,
+      logging: console.log,  // Adicionando logs para verificar a execução
+    }
+  );
 
-module.exports = sequelize;
-
+  module.exports = sequelize;
+  console.log('Conexão com o banco de dados bem-sucedida!');
+} catch (error) {
+  console.error('Erro ao conectar ao banco de dados:', error);
+}
